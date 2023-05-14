aededcaa2be0e67d4e9196553fd722f936e60b72
Julieta Ailyn Mosquera <julietaailyn@gmail.com>
2023-05-14 11:34:00 -0300

    cambios en railway

--- front/src/App.js
+++ front/src/App.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import LoginPage from './coomponents/LoginPages/LoginPage';
-// import EditProduct from './coomponents/EditProduct/EditProduct';
-// import store from './store';
-// import ProductList from './coomponents/Home/ProductList';
-// import AddProductForm from './coomponents/AddProduct/AddProductForm';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-
-// function App() {
-//   return (
-//       <Provider store={store}>
-//       <Router>
-//       {/* <ToastContainer/> */}
-//         <Routes>
-//         <Route exact path='/' element={<LoginPage/>}/>
-//         <Route exact path='/productList' element={<ProductList/>}/>
-//         <Route exact path="/add-producto" element={<AddProductForm/>} />
-//         <Route exact path='/editar-producto/:id' element={<EditProduct/>}/>
-//         </Routes>
-//       </Router>
-//       <ToastContainer position='top-left'/>
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -40,7 +9,7 @@ import AddProductForm from './coomponents/AddProduct/AddProductForm';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://controlstock-production.up.railway.app/";
 
 function App() {
   return (
